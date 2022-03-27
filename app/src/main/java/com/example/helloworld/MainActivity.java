afa902adc1df7b48f4f1a1c0ef9caf558a5fd675
Pankaj <pankajgoyal.ibirdscollege@gmail.com>
2022-03-27 12:23:09 +0530

    Toast Example

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = findViewById(R.id.editTextTextPersonName);
+                Toast.makeText(getApplicationContext(),editText.getText().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
